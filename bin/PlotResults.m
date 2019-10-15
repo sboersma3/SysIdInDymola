8d12fd6bfc2e6a12d2ab9d0ff45f624f7d550fa5
sboersma3 <sjoerd.boersma@supergrid-institute.com>
2019-10-15 16:01:06 +0200

    update plots so that Hertz instead of rad/s are depicted

--- bin/PlotResults.m
+++ bin/PlotResults.m
@@ -122,7 +122,7 @@ ylabel('$\zeta_i$','interpreter','latex');
 subplot(2,1,2)
 stairs(unique(wn_i(:,kk),'stable'),'bo','linewidth',2);grid;hold on;
 stairs(unique(wnhat_i(:,kk),'stable'),'ro','linewidth',2);
-ylabel('$\omega_{n,i}$','interpreter','latex','fontsize',fontsize);
+ylabel('$\omega_{n,i}$ (Hz)','interpreter','latex','fontsize',fontsize);
 xlabel('\# of natural frequencies','interpreter','latex','fontsize',fontsize);
 title('true (blue) and estimated (red)','interpreter','latex','fontsize',fontsize)
 set(gca,'xtick',0:length(unique(wn_i(:,kk))))
