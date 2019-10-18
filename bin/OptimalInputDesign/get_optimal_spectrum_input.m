e96e230b3bf461392f9bbeb000b1847230f7b23a
sboersma3 <sjoerd.boersma@supergrid-institute.com>
2019-10-18 11:49:31 +0200

    replaced sinus parametrization with fir parametrization in the fir branch

--- bin/OptimalInputDesign/get_optimal_spectrum_input.m
+++ bin/OptimalInputDesign/get_optimal_spectrum_input.m
@@ -38,87 +38,86 @@ vecC  = (ops.c1*ones(1,Nw) + ops.c2*C)/2;
 % matrix with unit vectors
 BB    = eye(size(Mbar,1));
 
-if 1
-        
-    setlmis([])
-    
-    [~,~,sPHI]       = lmivar(2,[Nw 1]);
-    sPHIdiag         = diag(sPHI);
-    diagPHI          = lmivar(3,sPHIdiag);
-    blkdiagPHIs      = kron(sPHI,eye(nx));
-    [blkdiagPHI,~,~] = lmivar(3,blkdiagPHIs);
-    
-    
-    for kk=1:Np
-        lmiterm([-kk 1 1 0],ops.alpha(kk))
-        lmiterm([-kk 2 1 0],BB(:,CritPar(kk)))
-        lmiterm([-kk 2 2 0],Mbar)                       % N*C*X*C'
-        lmiterm([-kk 2 2 blkdiagPHI],M,1)               % N/vare/2 * sum ( PHI * real(Fuw*Fuw') ), with Fuw = dG/H
-    end
-    
-    lmiterm([-(Np+1),1,1,diagPHI],1,1)
-    
-    lmis           = getlmis;
-    options        = [1e-4,100,1e12,10,0];              %[accuracy, max_it, feas_rad, , ]
-    
-    [copt,xopt]    = mincx(lmis,vecC,options);
-    
-    % make sure you do not apply an infeasible solution
-    if ~isempty(xopt)
-        PHIopt      = dec2mat(lmis,xopt,1);
-    else
-        PHIopt      = eps*ones(Nw,1);
-    end
-    
-else
-    cvx_begin sdp;
-    
-    cvx_precision('default')
-    
-    %cvx_solver mosek_2
-    cvx_solver sdpt3
-    %cvx_solver sedumi
-    
-    % decision variables
-    variable PHI(Nw,1)
-    
-    % information matrix (inverse covariance matrix)
-    Pinv = Mbar + M * kron(PHI,eye(nx));
-    Pinv  = tril(Pinv)+tril(Pinv,-1)';                  % enforce symmetry
-    
-    % cost
-    minimize( vecC*PHI );
-    
-    % constraints
-    subject to
-    
-    % 1) upperbound variance
-    for kk=1:Np
-        [ops.alpha(kk) BB(:,CritPar(kk))' ; BB(:,CritPar(kk))  Pinv] >= 0;
-    end
-    
-    % 2) positivity constraint
-    PHI >= 0;
-    
-    cvx_end
-    
-    % make sure you do not apply an infeasible solution
-    if strcmp(cvx_status,'Solved') || strcmp(cvx_status,'Inaccurate/Solved')
-        PHIopt      = PHI;
-    else
-        PHIopt      = eps*ones(Nw,1);
-    end
-    
+
+[a,b,c,d] = ssdata(balreal(minreal(Fuz)));
+Q         = b*b';
+X         = dlyap(a,Q);
+Fo        = c*X*c' + d*d';
+vecFm     = [];
+for kk    = 1:Nw
+    Fmpos  = c*(a^kk)*X*c' + d*d';
+    Fmneg  = Fmpos';
+    Fm     = Fmpos + Fmneg;
+    vecFm  = [vecFm, Fm];
+end
+vecM      = (Nid/vare)*[Fo vecFm];
+
+
+[a,b,c,d] = ssdata(syshat.G);
+Q         = b*b';
+X         = dlyap(a,Q);
+Fo        = c*X*c' + d*d';
+vecFm     = [];
+for kk    = 1:Nw
+    Fmpos = c*(a^kk)*X*c'+c*(a^(kk-1))*b*d';
+    Fmneg = Fmpos';
+    Fm    = Fmpos + Fmneg;
+    vecFm = [vecFm, Fm];
+end
+vecC      = [Fo vecFm];
+
+
+cvx_begin sdp;
+cvx_precision('default')
+cvx_solver sdpt3
+
+% decision variables
+variable RB(Nw+1,1)              % crosscorrelation of FIR excitation
+variable Q(Nw,Nw) symmetric
+
+% information matrix (inverse covariance matrix)
+R      = Mbar;
+for kk = 1:Nw+1
+    MI = vecM(:,(kk-1)*nx+1:kk*nx);
+    R  = R + MI*RB(kk);
+end
+R      = tril(R)+tril(R,-1)';
+
+% cost
+minimize(vecC*RB);
+
+% constraints
+subject to
+
+% 1) upperbound variance
+for kk=1:Np
+    [ops.alpha(kk) BB(:,CritPar(kk))' ; BB(:,CritPar(kk))  R] >= 0;
 end
 
+% 2) positivity constraint
+B    = [1;zeros(Nw-1,1)];
+A    = [zeros(1,Nw-1),0;eye(Nw-1),zeros(Nw-1,1)];
+C    = RB(2:Nw+1)';
+D    = RB(1);
+
+[Q-A'*Q*A C'-A'*Q*B;
+    C-B'*Q*A D-B'*Q*B] >= 0;
+
+cvx_end
+
+PHIopt = RB';
+
 % check constraint on var(zet_i)
-P     = inv(Mbar + M * kron(PHIopt,eye(nx))); % covariance matrix
+Pinv      = Mbar;
+for kk    = 1:Nw+1
+    MI    = vecM(:,(kk-1)*nx+1:kk*nx);
+    Pinv  = Pinv + MI*RB(kk);
+end
+P     = inv(Pinv);
 diagP = diag(P);
 for kk=1:Np
     [diagP(CritPar(kk)) ops.alpha(kk)]
 end
-
-
 
 
 
