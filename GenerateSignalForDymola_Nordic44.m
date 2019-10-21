c3470552d9b0bef5ecd8d6209d96817aefebf86b
unknown <s.boersma@tudelft.nl>
2019-10-21 21:06:46 +0200

    update the generate...m functions, now you plot the complete excitation signal for inspection

--- GenerateSignalForDymola_Nordic44.m
+++ GenerateSignalForDymola_Nordic44.m
@@ -14,31 +14,26 @@ ops.w           = linspace(.1*2*pi,3*2*pi,30)'; % frequency grid
 
 Ai              = .05*ones(length(ops.w),1);    % amplitudes initial batch
 
-
-u = 0;
 rng(4)
-for kk=1:length(ops.w)
-   thetai(kk) = randn;
-   u          = u + Ai(kk)*sin(ops.w(kk)*(0:ops.h:ops.N*ops.ll)+thetai(kk)); 
-end
+thetai          = randn(1,length(ops.w));      % phase initial batch
 
-
-A            = zeros(ops.N,length(ops.w));
+A               = zeros(ops.N,length(ops.w));  % amplitudes all batches
 for kk=1:ops.K
     A(kk,:)     = Ai;
 end
 
 for kk=ops.K+1:ops.N
     if ops.Optimal
+        load(strcat(ops.directory,'OptimalInput.mat'))
         Aopt              = interp1(OptimalInput(:,1),OptimalInput(:,2),ops.w,'linear');  % project on earlier used frequency grid
         Aopt(isnan(Aopt)) = 0;
         A(kk,:)           = Aopt;
     else
-        A(kk,:)   = A(kk-1,:);
+        A(kk,:)           = A(kk-1,:);
     end
 end
-B      = repmat(ops.w,1,ops.N);
-C      = repmat(thetai',1,ops.N);
+B   = repmat(ops.w,1,ops.N);    % frequencies all batches
+C   = repmat(thetai',1,ops.N);  % phase all batches
 
 
 h   = ops.h;
@@ -49,8 +44,24 @@ K   = ops.K;
 
 save(strcat(ops.directory,'InputDymola.mat'),'A','B','C','h','ll','N','K')
 
+
+t     = 0:ops.h:ops.N*ops.ll-ops.h;
+nt    = floor(length(t)/size(A,1));
+T     = zeros(size(A,1),nt);
+for ll = 1:size(A,1)
+        T(ll,:) = t((ll-1)*nt+1:ll*nt);
+end
+U     = zeros(size(A,1),size(T,2));
+u     = [];
+for ll = 1:size(A,1)
+    for kk=1:length(ops.w)
+        U(ll,:) = U(ll,:) + A(ll,kk)*sin(ops.w(kk)*T(ll,:)+thetai(kk));
+    end
+    u     = [u U(ll,:)];
+end
+
+
 ops.w = ops.w/2/pi;    
-t     = 0:ops.h:ops.N*ops.ll;
 U     = fft(u);
 N     = length(u);                 
 f     = (0:N-1)*(1/ops.h/N);   
@@ -79,4 +90,5 @@ xlabel('$\omega$ (Hz)','interpreter','latex')
 xlim([0 ops.w(end)])
 ylim([0 max(P)+0.5*max(P)])
 title('Excitation signal power','interpreter','latex')
+
 
--- GenerateSignalForDymola_SingleMachineInfiniteBus.m
+++ GenerateSignalForDymola_SingleMachineInfiniteBus.m
@@ -7,17 +7,17 @@ ops.Optimal     = 0;         % generate optimal signal (true) or base signal (fa
 ops.N           = 1;         % number of batches (even number or 1)
 ops.ll          = 10;        % every ll second the model will be linearized
 ops.h           = .01;       % sample period
-ops.K           = 1;         % after every K batch, the excitation signal is changed
+ops.K           = 2;         % after every K batch, the excitation signal is changed
 if ops.K>ops.N;ops.K=ops.N;end
 
 ops.w           = linspace(.01*2*pi,10*2*pi,60)'; % frequency grid
 
 Ai              = .2*ones(length(ops.w),1);    % amplitudes initial batch
+
 rng(4)
-thetai          = randn(1,length(ops.w));
+thetai          = randn(1,length(ops.w));      % phase initial batch
 
-
-A               = zeros(ops.N,length(ops.w));
+A               = zeros(ops.N,length(ops.w));  % amplitudes all batches
 for kk=1:ops.K
     A(kk,:)     = Ai;
 end
@@ -32,8 +32,8 @@ for kk=ops.K+1:ops.N
         A(kk,:)           = A(kk-1,:);
     end
 end
-B   = repmat(ops.w,1,ops.N);
-C   = repmat(thetai',1,ops.N);
+B   = repmat(ops.w,1,ops.N);    % frequencies all batches
+C   = repmat(thetai',1,ops.N);  % phase all batches
 
 
 h   = ops.h;
@@ -44,8 +44,13 @@ K   = ops.K;
 
 save(strcat(ops.directory,'InputDymola.mat'),'A','B','C','h','ll','N','K')
 
-t     = 0:ops.h:ops.N*ops.ll;
-T     = [t(1:ceil(length(t)/2));[t(ceil(length(t)/2)+1:end) t(end)+h]];
+
+t     = 0:ops.h:ops.N*ops.ll-ops.h;
+nt    = floor(length(t)/size(A,1));
+T     = zeros(size(A,1),nt);
+for ll = 1:size(A,1)
+        T(ll,:) = t((ll-1)*nt+1:ll*nt);
+end
 U     = zeros(size(A,1),size(T,2));
 u     = [];
 for ll = 1:size(A,1)
@@ -54,7 +59,6 @@ for ll = 1:size(A,1)
     end
     u     = [u U(ll,:)];
 end
-
 
 
 ops.w = ops.w/2/pi;    
