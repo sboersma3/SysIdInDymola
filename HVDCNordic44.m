4d6c2ea4548b1a007cc30c2db5de2513309400c0
sboersma3 <sjoerd.boersma@supergrid-institute.com>
2019-10-30 16:07:43 +0100

    update gitignore

--- HVDCNordic44.m
+++ HVDCNordic44.m
@@ -11,16 +11,16 @@ ops.Ny         = 3;                     % choice of your output for identificati
 ops.mode       = 'min';                 % mode of the true system you plot 
 ops.modeid     = 'min';                 % mode of the estimated system you plot ('min' if you want to plot the minimum)
 ops.na         = 11;                    % #of states of estimated model
-ops.nb         = ops.na+1;              % #of parameters in numerator G
+ops.nb         = ops.na-1;              % #of parameters in numerator G
 ops.nc         = ops.na;                % #of parameters in numerator H
 ops.nk         = 0;                     % #of delays in G
 ops.nu         = 2;                     % choice of your input channel from Dymola linearization (taken from u)
 ops.ne         = 4;                     % choice of your noise channel channel from Dymola linearization (taken from u) 
 ops.ny         = ops.Ny-2;              % choice of your output channel from Dymola linearization (taken from y) !make sure this output is equivalent to signals taken from data_2! 
 
-ops.h_new      = 0.04;                  % new sampling period after resampling (0 -> no resampling)
+ops.h_new      = 0.1;                  % new sampling period after resampling (0 -> no resampling)
 
-ops.w          = linspace(.1*2*pi,2*2*pi,30);        % frequency grid
+ops.w          = linspace(.3*2*pi,2*2*pi,30);        % frequency grid
 
 ops.c1         = 1;                                  % weighting factor for input (power or variance)
 ops.c2         = (1-ops.c1);                         % weighting factor for output (power)
