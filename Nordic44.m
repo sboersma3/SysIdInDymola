88e5aa6efc9adc1da3c6058f5a05697ee5c1b3f0
sboersma3 <sjoerd.boersma@supergrid-institute.com>
2020-01-24 17:28:34 +0100

    new sysid settings for N44 case

--- Nordic44.m
+++ Nordic44.m
@@ -3,16 +3,16 @@
 %% Nordic 44
 clear;clc
 
-ops.directory  = 'results\Nordic44\d\900\';
+ops.directory  = 'results\Nordic44\';
  
 % y = dphi and u = [Pref Qref eP5301 eQ5301]^T
 % data_2 = [t, Qref, dphi]
 ops.Ny         = 3;                     % choice of your output for identification (taken from data_2)
 ops.mode       = 'min';                 % mode of the true system you plot 
 ops.modeid     = 'min';                 % mode of the estimated system you plot ('min' if you want to plot the minimum)
-ops.na         = 8;                     % #of states of estimated model
-ops.nb         = ops.na;                % #of parameters in numerator G
-ops.nc         = ops.na-2;              % #of parameters in numerator H
+ops.na         = 6;                     % #of states of estimated model
+ops.nb         = ops.na-3;                % #of parameters in numerator G
+ops.nc         = ops.na-3;              % #of parameters in numerator H
 ops.nk         = 0;                     % #of delays in G
 ops.nu         = 2;                     % choice of your input channel from Dymola linearization (taken from u)
 ops.ne         = 4;                     % choice of your noise channel channel from Dymola linearization (taken from u) 
@@ -24,7 +24,6 @@ ops.w          = linspace(.1*2*pi,3*2*pi,30);        % frequency grid in optimal
 
 ops.c1         = 1;                                % weighting factor on input power
 ops.c2         = (1-ops.c1);                         % weighting factor on output power
-
 
 run('bin/MAIN')
 
