289bfb1fbe7536cd0671e9bc7c79825631d8a312
unknown <sjoerd.boersma@supergrid-institute.com>
2020-03-26 04:53:12 -0400

    push identified HVDC kundur

--- GenerateSignalForDymola_HvdcKundur.m
+++ GenerateSignalForDymola_HvdcKundur.m
@@ -6,11 +6,12 @@ ops.directory   = 'results\HvdcKundur\';
 
 ops.Optimal     = 0;         % generate optimal signal (true) or base signal (false)
 
-ops.N           = 3;         % number of batches (even number)
-ops.ll          = 40;        % every ll second the model will be linearized
+ops.N           = 2;         % number of batches (even number)
+ops.ll          = 60;        % every ll second the model will be linearized
 ops.h           = .005;       % sample period
 
-ops.w           = linspace(.1*2*pi,5*2*pi,20)';   % frequency grid
+ops.w           = linspace(.1*2*pi,3*2*pi,20)';   % frequency grid
+ops.w           = (.1*2*pi:1*.1*2*pi:3*2*pi)';
 Ai              = 0.08*ones(length(ops.w),1);         % amplitudes initial batch
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
